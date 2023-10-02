41c7d9eabc3e826c7a235ce2e76090ed2bf127b7
ssisaias <ssisaias@users.noreply.github.com>
2023-10-02 00:06:21 -0300

    Add links to card

--- src/components/WebsiteCard/CardFooterButtons/styles.ts
+++ src/components/WebsiteCard/CardFooterButtons/styles.ts
@@ -1,10 +1,29 @@
 import { styled } from "styled-components";
 
-export const CardFooterContainer = styled.div`
+export const WSCardFooterContainer = styled.div`
     display: flex;
-    flex-direction: row;
-    width: 13rem;
-    height: auto;
-    align-items: center; 
-    margin-left: 1.5rem;
+    justify-content: space-evenly;
+
+    a:link { text-decoration: none; }
+    a:visited { text-decoration: none; }
+
+    a{
+        display: flex;
+        border-radius: 4px;
+        cursor: pointer;
+        margin-bottom: 12px;
+        width: 40%;
+        justify-content: center;
+        align-items: center;
+        background: ${prosp => prosp.theme['yellow-light']};
+        
+
+        gap: 6px;
+
+        span{
+            font-family: 'Baloo 2';
+            font-size: 1.2rem;
+        }
+    }
+
 `
--- src/components/WebsiteCard/index.tsx
+++ src/components/WebsiteCard/index.tsx
@@ -1,6 +1,9 @@
 import { WebsiteCardContainer, WsCardDescriptionContainer } from "./styles"
 import { CardInterface } from "../../interface/interfaces"
 import { CardHeaderImg } from "./CardImgHeader/styles"
+import { WSCardFooterContainer } from "./CardFooterButtons/styles"
+import { ArrowSquareOut, GitFork } from "@phosphor-icons/react"
+import { Link } from "react-router-dom"
 
 export interface WebsiteCardProps {
   cardItem: CardInterface
@@ -21,6 +24,22 @@ export function WebsiteCard({ cardItem }: WebsiteCardProps) {
       <WsCardDescriptionContainer>
         <h4>{cardItem.description}</h4>
       </WsCardDescriptionContainer>
+
+      <WSCardFooterContainer>
+
+        
+          <Link to={cardItem.github || ""} target="_blank">
+            <span>Source</span>
+            <GitFork color="" size={18} />
+          </Link>
+
+        
+          <Link to={cardItem.sitelink || ""} target="_blank">
+            <span>Preview</span>
+            <ArrowSquareOut color="" size={18} />
+          </Link>
+
+      </WSCardFooterContainer>
     </WebsiteCardContainer>
   )
 }
